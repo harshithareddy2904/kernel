504733c42cfebd04dbb88d7d66cf5c3d7a05dee1
Harshitha Nagaraja Reddy <harshitha.reddy@oss.qualcomm.com>
2026-07-02 11:57:06 +0530

    Bluetooth: btqca: Support downloading board id specific NVM for QCC2072
      Download board id specific NVM instead of default for QCC2072 if board id
      is available.
    
    Change-Id: Idb3362c5a435456839c7586cd2b0c8c2420fc7ee
    Signed-off-by: Harshitha Nagaraja Reddy <harshitha.reddy@oss.qualcomm.com>

--- drivers/bluetooth/btqca.c
+++ drivers/bluetooth/btqca.c
@@ -927,7 +927,7 @@ int qca_uart_setup(struct hci_dev *hdev, uint8_t baudrate,
 	/* Give the controller some time to get ready to receive the NVM */
 	msleep(10);
 
-	if (soc_type == QCA_QCA2066 || soc_type == QCA_WCN7850)
+	if (soc_type == QCA_QCA2066 || soc_type == QCA_WCN7850 || soc_type == QCA_QCC2072)
 		qca_read_fw_board_id(hdev, &boardid);
 
 	/* Download NVM configuration */
@@ -988,8 +988,8 @@ int qca_uart_setup(struct hci_dev *hdev, uint8_t baudrate,
 				 "hmtnv", soc_type, ver, rom_ver, boardid);
 			break;
 		case QCA_QCC2072:
-			snprintf(config.fwname, sizeof(config.fwname),
-				 "qca/ornnv%02x.bin", rom_ver);
+			qca_get_nvm_name_by_board(config.fwname, sizeof(config.fwname),
+				 "ornnv", soc_type, ver, rom_ver, boardid);
 			break;
 		default:
 			snprintf(config.fwname, sizeof(config.fwname),
